6c2d75fd3fe5e8f980e8841582dc0318bf981781
Carlos Quillupangui <40344647+CarlitosBeto@users.noreply.github.com>
2023-05-29 10:32:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+// Endpoint para recibir el webhook de HubSpot
+app.post('/hubspot-webhook', (req, res) => {
+  // Verificar que la solicitud contiene datos del webhook
+  if (req.body) {
+    // Procesar los datos recibidos del webhook
+    const webhookData = req.body;
+    console.log('Datos del webhook de HubSpot:', webhookData);
+
+    // Realizar acciones adicionales según tus necesidades
+    // ...
+
+    // Responder al webhook de HubSpot con un código de estado exitoso (200)
+    res.sendStatus(200);
+  } else {
+    // Si no hay datos en la solicitud, responder con un código de estado de error (400)
+    res.sendStatus(400);
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`El servidor está escuchando en el puerto ${port}`);
 });
